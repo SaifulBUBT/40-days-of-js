cb07f94e92b4be1a76df62af774cdf0041542f3c
Md. Saiful Islam <42247667+SaifulBUBT@users.noreply.github.com>
2025-09-24 07:48:57 +0600

    js event listener

--- Day-19 (JavaScript Events)/Projects/faq.js
+++ Day-19 (JavaScript Events)/Projects/faq.js
@@ -1,23 +1,22 @@
-const faq = document.querySelector('.faq');
-faq.addEventListener('click', (e) => {
-    // const currentItem =  e.target.parentElement;
-    // const currentAnswer = currentItem.querySelector('.answer');
-    // // console.log(currentAnswer);
-    // currentAnswer.classList.toggle('show')
-    
-    const currentItem = e.target.parentElement;
-    const currentAnswer = currentItem.querySelector('.answer');
-    if (currentAnswer) {
-        currentAnswer.classList.toggle('show');
-    }
-    
-});
+const faq = document.querySelector(".faq");
+faq.addEventListener("click", (e) => {
+  // const currentItem =  e.target.parentElement;
+  // const currentAnswer = currentItem.querySelector('.answer');
+  // // console.log(currentAnswer);
+  // currentAnswer.classList.toggle('show')
 
-document.addEventListener('click', (e) => {
-    const allAnswers = document.querySelectorAll('.answer.show');
-    console.log(allAnswers);
-    
-    allAnswers.forEach(answer => {
-        answer.classList.remove('show');
-    })
+  const currentItem = e.target.parentElement;
+  const currentAnswer = currentItem.querySelector(".answer");
+  if (currentAnswer) {
+    currentAnswer.classList.toggle("show");
+  }
+});
+// event listener
+document.addEventListener("click", (e) => {
+  const allAnswers = document.querySelectorAll(".answer.show");
+  console.log(allAnswers);
+
+  allAnswers.forEach((answer) => {
+    answer.classList.remove("show");
+  });
 });
